e538aac720e7121e38e845ff79a424179d019413
Alex Levchuk <alexlevchuk@mail.ru>
2023-03-22 17:14:29 +0300

    Адаптив секций header и footer Адаптив index.html

--- src/js/main.js
+++ src/js/main.js
@@ -30,6 +30,7 @@ const menuBtn = document.querySelector('.menu__btn');
 const menuList = document.querySelector('.menu__list');
 
 menuBtn.addEventListener('click', () => {
+	menuBtn.classList.toggle('menu__btn--open');
 	menuList.classList.toggle('menu__list--active');
 });
 
